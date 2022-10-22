1ea5e6cb5d0e7bf6cad9965b0e3412d3f8560813
shun198 <shunhiroseluvmri@gmail.com>
2022-10-22 16:23:55 +0900

    初期設定の完了

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+poetry run python manage.py makemigrations --noinput
+poetry run python manage.py migrate --noinput
+poetry run python manage.py collectstatic --noinput
 
-if [ $DEBUG = 1 ]; then
-    python manage.py runserver 0.0.0.0:8000
+if [ $DEBUG = "True" ]; then
+    poetry run python manage.py runserver 0.0.0.0:8000
 else
-    gunicorn djangopj.wsgi:application --bind 0.0.0.0:8000
+    poetry run gunicorn api.wsgi:application --bind 0.0.0.0:8000
 fi
